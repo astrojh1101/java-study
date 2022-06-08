077f76772cd07d3bdd36d1a4fad8358729d7ae2a
astrojh1101 <daniel991101@naver.com>
2022-06-08 09:14:38 +0900

    chap03
    practice

---
+++ practice05.java
@@ -0,0 +1,6 @@
+public class practice05 {
+    public static void main(String[] args) {
+        int num = 333;
+        System.out.println((num/10*10)+1);
+    }
+}
---
+++ practice06.java
@@ -0,0 +1,7 @@
+public class practice06
+{
+    public static void main(String[] args) {
+        int num = 24;
+        System.out.println(10-(num%10));
+    }
+}
