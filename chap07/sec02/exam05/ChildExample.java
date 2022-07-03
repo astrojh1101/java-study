538920ca25e5635f769b42e1f5efefe43bb08e44
astrojh1101 <daniel991101@naver.com>
2022-07-03 17:09:11 +0900

    Chap0702 Study
    Chap0702 Study

---
+++ chap07/sec02/exam05/ChildExample.java
@@ -0,0 +1,10 @@
+package chap07.sec02.exam05;
+
+public class ChildExample {
+    public static void main(String[] args) {
+        Parent parent = new Child();
+        parent.field1 = "data1";
+        parent.method1();
+        parent.method2();
+    }
+}
