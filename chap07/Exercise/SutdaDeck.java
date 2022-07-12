51ed6ded1d84efa47bade988e437457695c374bb
astrojh1101 <daniel991101@naver.com>
2022-07-12 23:58:59 +0900

    Exercise0701
    unsolve yet

---
+++ chap07/Exercise/SutdaDeck.java
@@ -0,0 +1,44 @@
+package chap07.Exercise;
+
+public class SutdaDeck {
+    final int CARD_NUM = 20;
+    SutdaCard[] cards = new SutdaCard(CARD_NUM);
+
+    SutdaDeck() {
+        for(int i=0; i<CARD_NUM; i++) {
+            if(i==0 || i==2 || i==7) {
+                cards[i] = new SutdaCard(i+1, true);
+            } else {
+                cards[i] = new SutdaCard(i%10 +1, false);
+            }
+        }
+    }
+
+    class SutdaCard {
+        int num;
+        boolean isKwang;
+
+        SutdaCard() {
+            this(1, true);
+        }
+
+        SutdaCard(int num, boolean isKwang) {
+            this.num = num;
+            this.isKwang = isKwang;
+        }
+
+        //info 대신 Object클래스의 toString()을 오버라이딩했다.
+        public String toString() {
+            return num + ( isKwang ? "K" : "");
+        }
+    }
+
+    class Exercise0701 {
+        public static void main(String[] args) {
+            SutdaDeck deck = new SutdaDeck();
+
+            for(int i=0; i< deck.cards.length; i++)
+                System.out.print(deck.cards[i]+",");
+        }
+    }
+}
