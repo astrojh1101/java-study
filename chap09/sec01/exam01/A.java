03d54242c2f23cf591f3843255bcc374a5cbe7a0
astrojh1101 <daniel991101@naver.com>
2022-07-16 20:25:16 +0900

    Update A.java
    chap0901 review

--- chap09/sec01/exam01/A.java
+++ chap09/sec01/exam01/A.java
@@ -3,12 +3,12 @@ package chap09.sec01.exam01;
 public class A {
     A() {
         System.out.println("A 객체가 생성됨");
-    }
+    } // 바깥 클래스
 
     class B {
         B() {
             System.out.println("B 객체가 생성됨");
-        }
+        } // 인스턴스 멤버 클래스
         int field1;
         //static int field2;
         void method1() {}
@@ -18,7 +18,7 @@ public class A {
     static class C  {
         C() {
             System.out.println("C 객체가 생성됨");
-        }
+        } // 정적 멤버 클래스
         int field1;
         static int field2;
         void method1() {}
